13308f383ca1a5e60bb83243793795d7a65e9dc8
noman-khalid-dev <noman.khalid.freelance@gmail.com>
2021-01-20 16:13:57 +0500

    Adding stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(
@@ -22,9 +21,14 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
 
-class DicePage extends StatelessWidget {
-  // This widget is the root of your application.
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber=6;
+  int rightDiceNumber=1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,15 +36,21 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
               onPressed: (){
+                setState(() {
+                  leftDiceNumber=Random().nextInt(6)+1;
+                });
               },
             ),
           ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: (){
+                setState(() {
+                  rightDiceNumber=Random().nextInt(6)+1;
+                });
               },
             ),
           ),
@@ -48,4 +58,5 @@ class DicePage extends StatelessWidget {
       ),
     );
   } //build
-} //DicePage
+}//_DicePageState
+
