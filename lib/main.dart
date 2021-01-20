5dbeb163d3fc2597782cb805dc02b01bf2131546
noman-khalid-dev <noman.khalid.freelance@gmail.com>
2021-01-20 16:31:21 +0500

    use Math library and functions in dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,29 +29,39 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber=6;
   int rightDiceNumber=1;
+  void  changeNumber(){
+    setState(() {
+      leftDiceNumber=Random().nextInt(6)+1;
+      rightDiceNumber=Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-              onPressed: (){
-                setState(() {
-                  leftDiceNumber=Random().nextInt(6)+1;
-                });
-              },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlatButton(
+                padding: EdgeInsets.all(0.0),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: (){
+                  changeNumber();
+                },
+              ),
             ),
           ),
           Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-              onPressed: (){
-                setState(() {
-                  rightDiceNumber=Random().nextInt(6)+1;
-                });
-              },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlatButton(
+                padding: EdgeInsets.all(0.0),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: (){
+                  changeNumber();
+                },
+              ),
             ),
           ),
         ],
